af8fb6f2ed3855502f4fe4f24a13beca74b8a689
jennieyang95 <jennieyang95@gmail.com>
2021-03-11 16:17:02 -0800

    Initial commit

---
+++ app/src/main/java/com/bcit/lab5pages1to4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bcit.lab5pages1to4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
